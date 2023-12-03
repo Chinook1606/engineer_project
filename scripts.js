9bc147f482dc6c39b93a37e640771482b5ab2db2
Piotrek <piotr.bak@student.up.krakow.pl>
2023-12-03 02:31:54 +0100

    Cleaned mess with duplicated code in css and some small fixes in index.html

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var menuIcon = document.querySelector(".navbar__menu-icon");
+//   var navLinks = document.querySelector(".navbar__menu-list");
+
+//   menuIcon.addEventListener("click", function () {
+//     console.log("Menu icon clicked");
+//     navLinks.classList.toggle("hidden");
+//   });
+// });
